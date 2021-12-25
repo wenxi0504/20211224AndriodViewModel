619fd9d9aa2f733a308e0e7b43dfc7b7aa5050bc
Sophie <wenxi1988@hotmail.com>
2021-12-24 20:50:46 -0500

    Way 2, to access the SharedPrederences by using Context
    class

--- app/src/main/java/com/example/a20211224andriodviewmodel/MainActivity.java
+++ app/src/main/java/com/example/a20211224andriodviewmodel/MainActivity.java
@@ -5,10 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    MyViewModel myViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 1.1 In order to Access global resource files
+       // myViewModel.application=getApplication();
+
+        //2.0 in order to access global resource files bc:Applcation is inherited from Context
+        myViewModel.context = getApplicationContext();
     }
 }
--- app/src/main/java/com/example/a20211224andriodviewmodel/MyViewModel.java
+++ app/src/main/java/com/example/a20211224andriodviewmodel/MyViewModel.java
@@ -1,2 +1,23 @@
-package com.example.a20211224andriodviewmodel;public class MyViewModel {
+package com.example.a20211224andriodviewmodel;
+
+import android.app.Application;
+import android.content.Context;
+
+import androidx.lifecycle.ViewModel;
+
+public class MyViewModel extends ViewModel {
+    //1.0 in order to access global resource files
+    //Application application;
+
+    //2.0 in order to access global resource files bc:Applcation is inherited from Context
+    Context context;
+    //2.1 to get sharedPreferences
+    /*
+    void foo()
+    {
+        context.getSharedPreferences();
+    }
+     */
+
+
 }
