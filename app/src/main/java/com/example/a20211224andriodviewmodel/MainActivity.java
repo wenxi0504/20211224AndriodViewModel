eb047fde8e8f5a770bfef79f7e09984755a95b47
Sophie <wenxi1988@hotmail.com>
2021-12-24 21:44:48 -0500

    In MainActivity , set data binding

--- app/src/main/java/com/example/a20211224andriodviewmodel/MainActivity.java
+++ app/src/main/java/com/example/a20211224andriodviewmodel/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.a20211224andriodviewmodel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.SavedStateViewModelFactory;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
+import com.example.a20211224andriodviewmodel.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
     MyViewModel myViewModel;
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
        // myViewModel.application=getApplication();
 
         //2.0 in order to access global resource files bc:Applcation is inherited from Context
-        myViewModel.context = getApplicationContext();
+        //myViewModel.context = getApplicationContext();
+
+
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+        myViewModel= new ViewModelProvider(this,new SavedStateViewModelFactory(getApplication(),this)).get(MyViewModel.class);
+        binding.setData(myViewModel);
+        binding.setLifecycleOwner(this);
     }
 }
